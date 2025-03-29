6ef50b84d06928ae047619f428407144ba622d9d
samueleangel <samueleangel@gmail.com>
2025-03-29 10:00:39 +0100

    Fix

--- _printf.c
+++ _printf.c
@@ -9,48 +9,48 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0, i = 0;
+va_list args;
+int count = 0, i = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				i++;
-				if (!format[i])
-					break;
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (!format[i]) /* Handle single % at end */
+                break;
 
-				switch (format[i])
-				{
-					case 'c':
-						count += print_char(args);
-						break;
-					case 's':
-						count += print_string(args);
-						break;
-					default:
-						write(1, &format[i - 1], 1);
-						write(1, &format[i], 1);
-						count += 2;
-						break;
-				}
-			}
-			else
-			{
-				write(1, &format[i], 1);
-				count++;
-			}
-			i++;
-		}
-		va_end(args);
-		return (count);
-	}
+            switch (format[i])
+            {
+                case 'c':
+                    count += print_char(args);
+                    break;
+                case 's':
+                    count += print_string(args);
+                    break;
+                case '%':
+                    count += print_percent(args);
+                    break;
+                default:
+                    write(1, &format[i - 1], 1);
+                    write(1, &format[i], 1);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
 }
--- print_functions.c
+++ print_functions.c
@@ -11,7 +11,7 @@ int print_char(va_list args)
 {
 	char c = (char)va_arg(args, int);
 
-	if (write(1, &c, 1) == -1);
+	if (write(1, &c, 1) == -1)
 		return -1;
 	return (1);
 }
